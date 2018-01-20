2cc10a2cf4136ca83e1fa5285311ae5db2bcb717
Robert Gallagher <robertg@heanet.ie>
2018-01-20 21:39:39 +0000

    Fixing remote state config

--- prod/app/tf-remote-state.tf
+++ prod/app/tf-remote-state.tf
@@ -1,38 +1,25 @@
+# Default region
+#
+provider "aws" {
+  region = "eu-west-1"
+}
+
 # Configure terraform to use the remote state file
 #
 terraform {
  backend "s3" {
   encrypt = true
   bucket = "spoofedpacket-tf-remote-state-storage-s3"
-  dynamodb_table = "spoofedpacket-tf-state-lock-dynamo"
+  dynamodb_table = "tf-moodle-prod-app-state-lock-dynamo"
   region = "eu-west-1"
   key = "tf-moodle-prod-app.tfstate"
  }
 }
 
-# S3 bucket for the remote state file
-#
-resource "aws_s3_bucket" "spoofedpacket-tf-state-storage-s3" {
-  bucket = "spoofedpacket-tf-remote-state-storage-s3"
-
-  versioning {
-    enabled = true
-  } 
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags {
-    Name = "S3 Remote Terraform State Store"
-  }
-
-}
-
 # Create a dynamodb table for locking the state file
 #
-resource "aws_dynamodb_table" "spoofedpacket-dynamodb-tf-state-lock" {
-  name = "spoofedpacket-tf-state-lock-dynamo"
+resource "aws_dynamodb_table" "tf-moodle-prod-app-state-lock" {
+  name = "tf-moodle-prod-app-state-lock-dynamo"
   hash_key = "LockID"
   read_capacity = 20
   write_capacity = 20
